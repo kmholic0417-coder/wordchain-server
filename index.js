6945dbfd2eecfe4763e0de17a5d8df3f7acb6f71
kmholic0417-coder <kmholic0417@gmail.com>
2026-02-21 13:18:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log("서버 실행됨");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("서버 실행됨 on port " + PORT);
 });
